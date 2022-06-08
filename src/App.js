fd86ee66782e2cb22bcbe7ddc21c806effacfd53
Dominik Mrówka <dmrowka7@gmail.com>
2022-06-08 21:11:16 +0200

    Add Footer and NavBar

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import PostAdd from "./components/pages/PostAdd/PostAdd";
 import PostEdit from "./components/pages/PostEdit/PostEdit";
 import About from "./components/pages/About/About";
 import BlankPage from "./components/pages/BlankPage/BlankPage";
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+import { Container } from 'react-bootstrap';
 
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:id" element={<Post />} />
-        <Route path="/post/add" element={<PostAdd />} />
-        <Route path="/post/edit/:id" element={<PostEdit />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<BlankPage />} />
-      </Routes>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/post/add" element={<PostAdd />} />
+          <Route path="/post/edit/:id" element={<PostEdit />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<BlankPage />} />
+        </Routes>
+        <Footer />
+      </Container>
+
     </>
   );
 }
---
+++ src/components/views/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from "react-bootstrap";
+
+const NavBar = () => {
+     return (
+          <>
+               <Navbar bg="primary" variant="dark" className="rounded mt-4 mb-4">
+                    <Container>
+                         <Navbar.Brand href="/">Blog.app</Navbar.Brand>
+                         <Navbar.Collapse className="justify-content-end">
+                              <Navbar.Text>
+                                   <Nav className="me-auto">
+                                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                                        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                                   </Nav>
+                              </Navbar.Text>
+                         </Navbar.Collapse>
+                    </Container>
+               </Navbar>
+          </>
+     );
+}
+
+export default NavBar;
