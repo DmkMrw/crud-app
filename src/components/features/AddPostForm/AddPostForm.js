01706de604f5d7bec8f412a44f0c66ac2eef00b5
Dominik Mrówka <dmrowka7@gmail.com>
2022-06-25 19:03:36 +0200

    Problem

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -1,9 +1,13 @@
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addPost } from '../../../redux/postRedux';
 
 
 
 const AddPostForm = () => {
+
+     const dispatch = useDispatch();
 
      const [title, setTitle] = useState('');
      const [author, setAuthor] = useState('');
@@ -11,39 +15,41 @@ const AddPostForm = () => {
      const [description, setDescription] = useState('');
      const [content, setContent] = useState('');
 
-
+     const handleSubmit = (e) => {
+          e.preventDefault();
+          dispatch(addPost(title, author, publishedDate, description, content))
+     }
 
      return (
           <>
-               <h1>add post form</h1>
                <Row className='justify-content-md-center'>
                     <Col md={6}>
-                         <Form>
+                         <Form onSubmit={handleSubmit}>
                               <Col md={6}>
                                    <Form.Group className="mb-3" controlId="title">
                                         <Form.Label>Title</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter title"  />
+                                        <Form.Control type="text" placeholder="Enter title" onChange={(e) => setTitle(e.target.value)} />
                                    </Form.Group>
 
                                    <Form.Group className="mb-3" controlId="author">
                                         <Form.Label>Author</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter author"  />
+                                        <Form.Control type="text" placeholder="Enter author" onChange={(e)=> setAuthor(e.target.value)} />
                                    </Form.Group>
 
                                    <Form.Group className="mb-3" controlId="published">
                                         <Form.Label>Published</Form.Label>
-                                        <Form.Control type="date" placeholder="Enter date" />
+                                        <Form.Control type="date" placeholder="Enter date" onChange={(e)=> setPublishedDate(e.target.value)}/>
                                    </Form.Group>
                               </Col>
 
                               <Form.Group className="mb-3" controlId="description">
                                    <Form.Label>Short Description</Form.Label>
-                                   <Form.Control type="text" as="textarea" rows={3} placeholder="Leave a comment here" />
+                                   <Form.Control type="text" as="textarea" rows={3} placeholder="Leave a comment here" onChange={(e)=> setDescription(e.target.value)}/>
                               </Form.Group>
 
                               <Form.Group className="mb-3" controlId="content">
                                    <Form.Label>Main Content</Form.Label>
-                                   <Form.Control type="text" as="textarea" rows={7} placeholder="Leave a comment here" />
+                                   <Form.Control type="text" as="textarea" rows={7} placeholder="Leave a comment here" onChange={(e)=> setContent(e.target.value)}/>
                               </Form.Group>
 
                               <Button variant="primary" type="submit">
--- src/redux/postRedux.js
+++ src/redux/postRedux.js
@@ -1,18 +1,24 @@
+// import shortid from "shortid";
+
 //selectors
 
 
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
 const DELETE_POST = createActionName('DELETE_POST');
+const ADD_POST = createActionName('ADD_POST');
 
 
 // action creators
 
 export const deletePost = payload => ({ type: DELETE_POST, payload });
+export const addPost = payload => ({type: ADD_POST, payload })
 
 
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case ADD_POST:
+        return [...statePart, {...action.payload}] // dlaczego kropki przed action.payload
     case DELETE_POST:
         return statePart.filter(post => post.id !== action.payload);
     default:
