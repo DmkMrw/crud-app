dc62737e130c0bf4aa3a80948351b0b06d6d2bb8
Dominik Mrówka <dmrowka7@gmail.com>
2022-07-01 23:50:29 +0200

    Remove unnecessary component

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { Nav, ListGroup, Row, Col } from 'react-bootstrap';
 import shortid from "shortid";
-import Category from "../Category/Category";
 
 
 const Categories = () => {
